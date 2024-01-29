79eb248ab74d5f4ead093f8aaa5e9ef723c9fbb5
al-shaimon <alshaimon968@gmail.com>
2024-01-30 04:33:12 +0600

    number guessing game

--- projects/number-guessing-game.py
+++ projects/number-guessing-game.py
@@ -3,7 +3,7 @@ import random
 RandomNumber = random.randrange(1, 500)
 
 while True:
-    userInput = int(input("Guess the number: "))
+    userInput = int(input("Guess a number between 1 and 500: "))
     if userInput > RandomNumber:
         print("The number is too high!")
     elif userInput < RandomNumber:
